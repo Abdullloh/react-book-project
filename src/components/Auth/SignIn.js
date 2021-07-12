a774a0225499db34995470401ab7edb49616cce1
Abdullloh <abdullohjonirgashev@gmail.com>
2021-07-12 07:10:54 +0500

    auth page completed

---
+++ src/components/Auth/SignIn.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import Img from './img/SignIn.jpg'
+import {Link} from 'react-router-dom'
+import classes from './signIn.module.css'
+export default function SignIn() {
+    const[signIn,setSign] = useState({
+        email:'',
+        password:''
+    })
+    const handleInputChange = e =>{
+        const{name,value} = e.target;
+        console.log('clikde',name)
+        setSign({...signIn,[name]:value})
+    }
+    
+    const submitForm = (e) =>{
+        e.preventDefault()
+        const{email,password} = signIn;
+        const user = {
+            email,
+            password
+        }
+        var requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user),
+          };
+          fetch("http://book.alitechbot.uz/api/login", requestOptions)
+           .then(data=> data.json())
+           .then(data=>{
+               console.log(data);
+               if(data.success){
+                   alert('Succesfully Log In')
+                   window.location.pathname= './home'
+               }else{
+                   const {error} = data
+                   alert(error)
+               }
+           })
+
+    }
+    return (
+
+        <div className={classes.signInContainer}>
+            <div className={classes.colLeft}>
+                 <img src={Img} alt="leftCont" />
+            </div>
+            <div className={classes.colRight}>
+                  <div className={classes.signContainer}>
+                      <h1>Sign in</h1>
+                      <p>Do not you have an account?<Link to='./sign-up'>Sign up</Link></p>
+                      <form onSubmit={(e)=>submitForm(e)} action="#">
+                          <input onChange={handleInputChange} type="text" name='email' placeholder='Your email' />
+                          <input onChange={handleInputChange} type="text" name='password' placeholder='Your password' />
+                          <button className={classes.button}>Next step</button>
+                      </form>
+                  </div> 
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Auth/SignUp.js
@@ -0,0 +1,80 @@
+import React,{useState} from "react";
+import Img from "./img/SignUp.png";
+import { Link, Route} from "react-router-dom";
+import classes from "./signUp.module.css";
+import SignIn from "./SignIn";
+export default function SignUp() {
+  const [state,setState] = useState({
+    firstName:'',
+    lastName:'',
+    phone:'',
+    email:'',
+    password:''
+})
+console.log(state);
+ const handleInputChange = e=>{
+     const {name,value} = e.target
+     setState({...state,[name]:value})
+ }
+  function submitForm(e) {
+     e.preventDefault()
+     const{email,firstName,lastName,password} = state
+     const user = {
+      firstName,
+      lastName,
+      email,
+      password,
+     }
+     localStorage.setItem('users',JSON.stringify(user))
+     var requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    };
+     fetch("http://book.alitechbot.uz/api/sign-up", requestOptions)
+      .then(data=>data.json())
+      .then(result=>{
+        console.log(result);
+        if(result.success){
+          alert('Ruyhatdan muvofaqiyatli utdiz')
+          window.location.pathname='./home'
+        }else{
+          const{msg} = result;
+          alert(msg)
+        }
+        localStorage.setItem('token',result.token)
+      }
+      )
+      .catch(err=>console.log(err.msg))
+      
+   }
+  //  useEffect(()=>{
+  //    const{email,firstName,lastName,password} = state
+  //     localStorage.setItem('users',JSON.stringify({firstName:firstName,lastName:lastName,email:email,password:password}))
+  // },[])
+  return (
+    <div className={classes.signUpContainer}>
+      <div className={classes.colLeft}>
+        <img src={Img} alt="leftCont" />
+      </div>
+      <div className={classes.colRight}>
+        <div className={classes.signContainer}>
+          
+          <h1>Sign Up</h1>
+          <p>
+            Do not you have an account?<Link to="/sign-in">Sign in</Link>
+          </p>
+            <Route path="/sign-in" render={() => <SignIn />} />
+          <form  onSubmit={(e) => submitForm(e)}  className={classes.form} action="#">
+            <input onChange={handleInputChange} type="text" name="firstName" placeholder="First Name" />
+            <input onChange={handleInputChange} type="text" name="lastName" placeholder="Last Name" />
+            <input onChange={handleInputChange} type="tel" name="phone" placeholder="Phone" />
+            <input onChange={handleInputChange} type="email" name="email" placeholder="Email" />
+            <input onChange={handleInputChange} type="password" name="password" placeholder="Password" />
+            <button  className={classes.button}> Next step </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
